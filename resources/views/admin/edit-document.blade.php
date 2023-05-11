ab09da3561b11aedcfb81fdaacd094499fee5a4a
sanaafreen1 <sanaafreen2572@gmail.com>
2023-05-11 19:23:17 +0530

    edi doc
    edit doc

--- resources/views/admin/edit-document.blade.php
+++ resources/views/admin/edit-document.blade.php
@@ -2,20 +2,6 @@
 @section('content')
 
 
-                <h4 class="fw-bold py-3 mb-4">
-                    <span class=" fw-light"><strong> Link Documents to Services/సేవలకు పత్రాలను లింక్ చేయండి </strong>
-                    </span>
-                </h4>
-                 <form action="" method="post" id="link_doc" name="link_doc">
-
-                <div class="row mb-5">
-                    <div class="col-md-6 mb-3">
-                        <div class="form-group">
-                            Select Service / సేవను ఎంచుకోండి
-
-                            <select class="form-select" >
-                                <option value="">Select Service / సేవను ఎంచుకోండి</option>
-                                @foreach ($service as $services)
 
 
 <style>
@@ -68,56 +54,11 @@
 
 
                     </div>
-
-                    <div class="col-md-6 mb-3">
-                        <div class="form-group">
-                            Select Sub Service/ఉప సేవను ఎంచుకోండి
-
-                            <select class="form-select" >
-                                <option value="">Select Sub Service / సేవను ఎంచుకోండి</option>
-                                @foreach ($sub_service as $services)
-
-
-                                <option value="{{$services->sub_service_id}}" @if($services->service_id == $sub_service[0]->sub_service_id) selected ;@endif >{{$services->sub_service_name}}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                    </div>
-
-                    <div class="col-md-12 mb-3">
-                        <div class="form-group">
-                            <small>Select Documents / పత్రాలను ఎంచుకోండి</small>
-                            <ul class="m-0 p-0 s-doc-mobile">
-                               @foreach ($document as $data)
-                                <li class="align-items-start d-flex me-3 float-start">
-                                    <span class="p-1"><input type="checkbox" class="largerCheckbox mt-1" name="documents[]" value="{{$data->document_id}}"    /></span>
-                                    <span class="p-1" >{{$data->document_name}}</span>
-                                </li>
-                            </ul>
-                            @endforeach
-                        </div>
-                    </div>
+                    <!-- / Content -->
 
 
 
-                    <div class="col-md-2 mb-3 ms-auto">
-                        <div class="form-group text-end">
-                            <small>&nbsp;</small>
-                            <input type="button" class="btn btn-primary" value="Save / సేవ్ చేయండి" />
-                        </div>
-                    </div>
-                </div>
-              </div>
-
-
-        </div>
-    </div>
-</div>
-
-
-
-@endsection
-@push('scripts')
+ripts')
 
 <script>
     $(document).ready(function() {
